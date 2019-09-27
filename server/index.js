c729310fe766a982b9fbc32e66d915200ed01bea
Gus <gusta1296@gmail.com>
2019-09-26 23:22:20 -0700

    changed again

--- server/index.js
+++ server/index.js
@@ -2,15 +2,14 @@ const express = require('express');
 
 const app = express();
 
-const passport = require('passport');
 
-const GoogleStrategy = require('passport-google-oauth20');
+app.get('/', (req, res)=>{
+  res.send({ hi: 'there'});
+
+
+});
 
 const port = 5000;
-
-
-
-passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || port;
 
