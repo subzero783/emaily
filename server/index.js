d8ffac6832890b16d9412539ff492be7f7c20b1e
Gus <gusta1296@gmail.com>
2019-10-30 14:24:40 -0700

    worked

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
-
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
+passport.use(new GoogleStrategy());
 
-app.get('/', (req, res)=>{
-  res.send({ hi: 'there'});
+// client ID 858926767105-v5secnreg34n7h1u2ta2qin6b9pl4viv.apps.googleusercontent.com
+// client Secret 
 
-
-});
+// app.get('/', (req, res)=>{
+//   res.send({ hi: 'there'});
+// });
 
 const port = 5000;
-
 const PORT = process.env.PORT || port;
-
 app.listen(PORT);
