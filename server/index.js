df7c7f95b7c9578c37ccbedd474efa20d3912a69
Gus <gusta1296@gmail.com>
2019-10-30 21:14:20 -0700

    added google oauth

--- server/index.js
+++ server/index.js
@@ -1,16 +1,32 @@
+
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, accessToken => {
+    console.log(accessToken);
+  })
+);
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 // client ID 858926767105-v5secnreg34n7h1u2ta2qin6b9pl4viv.apps.googleusercontent.com
 // client Secret 
 
-// app.get('/', (req, res)=>{
-//   res.send({ hi: 'there'});
-// });
+app.get('/', (req, res)=>{
+  res.send({ hi: 'there'});
+});
 
 const port = 5000;
 const PORT = process.env.PORT || port;
