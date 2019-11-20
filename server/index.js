04a141e7d3ec6fae40cbde7e243481b8e5a1bb92
Gustavo Amezcua <gusta9753@gmail.com>
2019-11-19 22:58:07 -0800

    finished Connecting Mongoose to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
+
+mongoose.connect(
+  keys.mongoURI, 
+  {
+    useNewUrlParser: true,  
+    useUnifiedTopology: true
+  }
+);
 
 const app = express();
 
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
-
-mongoose.connect(keys.mongoURI);
-
 app.listen(PORT);
