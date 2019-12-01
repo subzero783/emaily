ec9223150ecc89a8749b785f70d9f291fd0d5414
Gus <gusta1296@gmail.com>
2019-11-30 21:33:55 -0800

    left at Dev vs Prod keys

--- server/services/passport.js
+++ server/services/passport.js
@@ -6,6 +6,17 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done)=>{
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done)=>{
+  User.findById(id)
+    .then(user=>{
+      done(null, user);
+    });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -19,7 +30,7 @@ passport.use(
           .then((existingUser)=>{
             if(existingUser){
               //we already have a record with the given profile id
-              done(null, existingUser);
+              done(null , existingUser);
             }else{
               //we don't have a user record with this ID, make a new record
               new User({ googleID: profile.id })
