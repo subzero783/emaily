d986323c60daf1cc6b007f3448d09404eec19561
Gus <gusta1296@gmail.com>
2020-01-25 21:21:26 -0800

    finished installing react client and adding proxy to use only the node server

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
     proxy('/auth/google',{
-      target: 'http://localhost:5000'
+      target: 'https://emaily-239.herokuapp.com'
     })
   );
 };
